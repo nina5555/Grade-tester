f05d18bdf503927a3f09e45684fb51369b10f077
Apt-get Root <AptgetRoot@users.noreply.github.com>
2016-12-26 14:15:15 +0100

    Update grade-tester.cpp

--- grade-tester.cpp
+++ grade-tester.cpp
@@ -1,273 +1,121 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#define N 100
 
 using namespace std;
 
+ofstream output("Summary.txt");
+
 int main()
 {
-    int push,choose;
-    char exit = 'n';
-    while ((exit!='n') || (exit!='N') || (exit!='No') || (exit!='no'))
-    {
-        float x,y,z,sommax,mediax,sommay,mediay,sommaxz,mediaxz,sommayz,mediayz,mediavoti,numerovoti,voti,somma=0;
-        char materia[20];
-        int a=0;
-        cout << "\n\n\t\t\tPROGRAM CREATED BY APT-GET ROOT\t\t\t";
-        cout <<"\n\n\t\t\t******************************";
-        cout << "\n\t\t\t*        GRADE TESTER        *";
-        cout <<"\n\t\t\t******************************";
-        cout << "\n\n\nInsert the subject of your grades: ";
-        cin >> materia;
-        cout << "\nInsert the total number of your grades: ";
-        cin >> numerovoti;
-        cout << endl;
-        while(a < numerovoti)
-        {
-            cout << "Insert the grades : ";
-            cin >> voti;
-            somma += voti;
-            a++;
-        }
-        float media = (somma/numerovoti);
-        cout << endl;
-        cout << "Subject : " << materia << endl;
-        cout << "Average grades :" << media << endl;
-        cout << endl;
-        cout << "Which average do you want reach? ";
-        cin >> mediavoti;
-        cout << endl;
-        cout << "1. to know which a single grade which could reach the average want.\n2. to know more number grade could make you reach the average want. ";
-        cout <<"\n\nDigit your choice: ";
-        cin >> choose;
-        cout << endl<< endl;
-        switch(choose)
-        {
-            case 1:
-            {
-                if(media < mediavoti)
-                {
-                    float x = 0.0;
-                    do
-                    {
-                        sommax = somma + x;
-                        mediax = sommax / ( numerovoti+1);
-                        x = x + 0.1;
-                        if(mediax >= mediavoti)
-                        {
-                            cout << "You have to get " << x << " to have the average grades greater to the average grades choose" << endl;
-                            cout << endl;
-                            cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                            cin >> push;
-                            if(push ==1)
-                            {
-                                cout << string(20, '\n');
-                                break;
-                            }
-                            if(push == 0)
-                            {
-                                return 0;
-                            }
-                        }
-                    }while(mediax <= mediavoti && x < 100.0);
-                }
-                if(media >= mediavoti);
-                {
-                    float y = 10.0;
-                    do
-                    {
-                        y = y - 0.1;
-                        sommay = somma + y;
-                        mediay = sommay / (numerovoti+1);
-                        if(mediay <= mediavoti)
-                        {
-                            cout << "You have not to get " << y << " otherwise your average grades go down the average grades choose" << endl;
-                            cout << endl;
-                            cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                            cin >> push;
-                            if(push ==1)
-                            {
-                                cout << string(20, '\n');
-                                break;
-                            }
-                            if(push == 0)
-                            {
-                                return 0;
-                            }
-                        }
-                    }while(mediay > mediavoti && y > -100.0);
-                }
-                break;
-            }
-            case 2:
-            {
-                if(media < mediavoti)
-                {
-                    float x = 0.0;
-                    float z = 0.0;
-                    do
-                    {
-                        sommax = somma + x;
-                        mediax = sommax / ( numerovoti + 1);
-                        x = x + 0.1;
-                        if(mediax >= mediavoti)
-                        {
-                            if(x <= 10)
-                            {
-                                cout << "You have to get " << x << " to have the average grades greater to the average grades choose" << endl;
-                                cout << endl;
-                                cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                                cin >> push;
-                                if(push ==1)
-                                {
-                                    cout << string(20, '\n');
-                                    break;
-                                }
-                                if(push == 0)
-                                {
-                                    return 0;
-                                }
-                            }
-                            if(x > 10)
-                            {
-                                do
-                                {
-                                    x = x - 0.1;
-                                }while(x >= 10.1);
-                                if(x <= 10.1)
-                                {
-                                    float z = 0.0;
-                                    do
-                                    {
-                                        z = z + 0.1;
-                                        sommaxz = (somma + x ) + z;
-                                        mediaxz = sommaxz / ( numerovoti + 2);
-                                        if(mediaxz >= mediavoti)
-                                        {
-                                            if(z > 10)
-                                            {
-                                                z = 10.0;
-                                                cout << "You have to get "<< x << " and " << z << " and 1 more grade to have the average grades greater to the average grades choose"<< endl;
-                                                cout << endl;
-                                                cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                                                cin >> push;
-                                                if(push ==1)
-                                                {
-                                                    cout << string(20, '\n');
-                                                    break;
-                                                }
-                                                if(push == 0)
-                                                {
-                                                    return 0;
-                                                }
-                                            }
-                                            if(z < 10)
-                                            {
-                                                cout << "You have to get " << x << " and " << z << " to have the average grades greater to the average grades choose" << endl;
-                                                cout << endl;
-                                                cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                                                cin >> push;
-                                                if(push ==1)
-                                                {
-                                                    cout << string(20, '\n');
-                                                    break;
-                                                }
-                                                if(push == 0)
-                                                {
-                                                    return 0;
-                                                }
-                                            }
-                                        }
-                                    }while(mediaxz <= mediavoti && z < 100.0);
-                                }
-                            }
-                        }
-                    }while(mediax <= mediavoti && x < 100.0 && z < 100.0);
-                    break;
-                }
-                if(media >= mediavoti);
-                {
-                    float y = 10.0;
-                    do
-                    {
-                        y = y - 0.1;
-                        sommay = somma + y;
-                        mediay = sommay / (numerovoti+1);
-                        if(mediay <= mediavoti)
-                        {
-                            if(y >= 0.0)
-                            {
-                                cout << "You have to get " << y << " to have the average grades greater to the average grades choose" << endl;
-                                cout << endl;
-                                cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                                cin >> push;
-                                if(push ==1)
-                                {
-                                    cout << string(20, '\n');
-                                    break;
-                                }
-                                if(push == 0)
-                                {
-                                    return 0;
-                                }
-                            }
-                            if(y < 0.0)
-                            {
-                                do
-                                {
-                                    y = y + 0.1;
-                                }while(y <= 0.0);
-                                if(y >= 0.0)
-                                {
-                                    float z = 10.0;
-                                    do
-                                    {
-                                        z = z - 0.1;
-                                        sommayz = (somma + y ) + z;
-                                        mediayz = sommayz / ( numerovoti + 2);
-                                        if(mediayz <= mediavoti)
-                                        {
-                                            if(z <= 0)
-                                            {
-                                                z = 0.0;
-                                                cout << "You have not to get "<< y << " and " << z << " and one more grade to have the average grades greater to the average grades choose"<< endl;
-                                                cout << endl;
-                                                cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                                                cin >> push;
-                                                if(push ==1)
-                                                {
-                                                    cout << string(20, '\n');
-                                                    break;
-                                                }
-                                                if(push == 0)
-                                                {
-                                                    return 0;
-                                                }
-                                            }
-                                            if(z > 0)
-                                            {
-                                                cout << "You have not to get " << y << " and " << z << " to have the average grades greater to the average grades choose" << endl;
-                                                cout << endl;
-                                                cout << "\n\n1. to return menu \n0. to exit\nDigit your choice: ";
-                                                cin >> push;
-                                                if(push ==1)
-                                                {
-                                                    cout << string(20, '\n');
-                                                    break;
-                                                }
-                                                if(push == 0)
-                                                {
-                                                    return 0;
-                                                }
-                                            }
-                                        }
-                                    }while(mediayz > mediavoti && y > -100.0 && z > -100.0);
-                                }
-                            }
-                        }
-                    }while(mediay > mediavoti && y > -100.0);
-                }
-                break;
-            }
-        }
-    }
-    return 0;
+	int push, choose;
+	char exit = 'n';
+	while ((exit != 'n') || (exit != 'N') || (exit != 'No') || (exit != 'no'))
+	{
+		float voti[N], somma = 0, media = 0, media_raggiungere = 0, numero_voti = 0;
+		char materia[1024];
+		cout << "\n\n\t\t\tPROGRAM CREATED BY APT-GET ROOT\t\t\t";
+		cout << "\n\n\t\t\t******************************";
+		cout << "\n\t\t\t*        GRADE TESTER        *";
+		cout << "\n\t\t\t******************************";
+		
+		cout << "\n\n\nInsert the subject of your grades: ";
+		cin >> materia;
+		output << "Subject : " << materia << endl;
+		cout << "\nInsert the total number of your grades: ";
+		cin >> numero_voti;
+		cout << endl;
+		output << "\nGrades : " << endl;
+		for (int a = 0; a < numero_voti; a++)
+		{
+			cout << "Insert the grades : ";
+			cin >> voti[a];
+			output << "Grade " << a + 1 << " : " << voti[a] << endl;
+			somma += voti[a];
+		}
+		media = (somma / numero_voti);
+		cout << "\nAverage grades :" << media << endl << endl;
+		output << "\nAverage grades : " << media << endl << endl;
+		cout << "Which average do you want reach [more or less than " << media << "] ? ";
+		cin >> media_raggiungere;
+		cout << endl;
+
+		if (media <= media_raggiungere)
+		{
+			cout << "\nYou have to get ";
+			output << "\nYou have to get ";
+			float numero_aggiunto = 1, nuova_media = 0, x = 12;
+			int a = 1;
+			while (x > 10.0)
+			{
+				x = 0.01;
+				nuova_media = 0;
+				for (float sommax = 0; media_raggiungere > nuova_media; x += 0.01)
+				{
+					sommax = somma + x;
+					nuova_media = (sommax / (numero_voti + numero_aggiunto));
+				}
+				if (a != 1)
+				{
+					cout << "10 e ";
+					output << "10 e ";
+				}
+				a++;
+				somma += 10.0;
+				numero_aggiunto++;
+			}
+			cout << x << " to have " << media_raggiungere << " \n" << endl;
+			output << x << " to have " << media_raggiungere << " \n" << endl;
+			cout << "\n\nSee the file \"Summary\" inside the directory to a review of all your subject\n\n1. To return menu \n0. To exit\nDigit your choice [ 1 or 0 ]: ";
+			cin >> push;
+			output << "\n\n";
+			if (push == 1)
+				cout << string(20, '\n');
+			if (push == 0)
+			{
+				output.close();
+				return 0;
+			}
+		}
+
+		if (media > media_raggiungere)
+		{
+			cout << "\nYou have to get ";
+			output << "\nYou have to get ";
+			float numero_aggiunto = 1, nuova_media = 10, x = -1.0;
+			int a = 1;
+			while (x < 0.0)
+			{
+				x = 10;
+				nuova_media = 10;
+				for (float sommax = 0; media_raggiungere < nuova_media; x -= 0.01)
+				{
+					sommax = somma + x;
+					nuova_media = (sommax / (numero_voti + numero_aggiunto));				
+				}
+				if (a != 1)
+				{
+					cout << "0 and ";
+					output << "0 and ";
+				}
+				a++;
+				numero_aggiunto++;
+			}
+			cout << x << " to have "<< media_raggiungere << " \n" << endl;
+			output << x << " to have " << media_raggiungere << " \n" << endl;
+			cout << "\n\nSee the file \"Summary\" inside the directory to a review of all your subject\n\n1. To return menu \n0. To exit\nDigit your choice [ 1 or 0 ]: ";
+			cin >> push;
+			output << "\n\n";
+			if (push == 1)
+				cout << string(20, '\n');
+			if (push == 0)
+			{
+				output.close();
+				return 0;
+			}
+		}
+	}
+	output.close();
+	return 0;
 }
